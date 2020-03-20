4cf4f19d283953e497c0b611e58c826591950fb7
Bulkan Evcimen <bulkan@gmail.com>
2020-03-20 20:13:21 +1100

    Initial Commit

---
+++ WaniKaniNotifier/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WaniKaniNotifier
+//
+//  Created by Bulkan Evcimen on 20/3/20.
+//  Copyright © 2020 Bulkan Evcimen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
